460ae60627a7f0488dd26a810f714b13f14877d7
Vincent <thisoneis4business@gmail.com>
2023-11-08 21:35:37 +1300

    private alpha

--- packages/app/src/routes/page.tsx
+++ packages/app/src/routes/page.tsx
@@ -11,7 +11,8 @@ const LandingPage = () => {
           <img className="w-5 h-5" src="/ai.svg" />
           <span className="font-cal text-lg text-black">ghostwriter</span>
           <span className="flex items-center justify-center px-2 py-0.5 rounded-full bg-purple-200 text-purple-900 text-xs">
-            alpha
+            <img className="w-3 h-3" src="/icons/lock.svg" />
+            <span className="ml-1 mr-0.5">alpha</span>
           </span>
         </div>
 
