26214247381b63eb0a5b9cf160f0cd5860e39e1d
Vincent <thisoneis4business@gmail.com>
2023-11-14 21:33:57 +1300

    feat: track usages (pricing)

--- packages/server/src/router.ts
+++ packages/server/src/router.ts
@@ -4,6 +4,7 @@ import { ulid } from "ulid";
 import { z } from "zod";
 import { Context } from "./context.js";
 import { productions } from "./data/content.js";
+import { sql } from "./data/pg.js";
 import { auth } from "./lib/lucia.js";
 import { produce } from "./production/index.js";
 
@@ -44,6 +45,10 @@ export const router = t.router({
         code: "UNAUTHORIZED",
         message: "You must be logged in to create content.",
       });
+
+    await sql("update users u set usages = usages + 1 where u.id = $1", [
+      ctx.session.user.id,
+    ]);
 
     const id = ulid();
     produce(id, input);
